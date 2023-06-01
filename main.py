4b3bcc7d277d41f645eacd4b374607427fee447c
kdevivanco <kdevivanco@gmail.com>
2023-06-01 07:48:09 -0500

    clean

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-# This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from config_files.config import get_connection
